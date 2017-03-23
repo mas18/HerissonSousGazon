1764774305cb3f3ed14fbd4f51509fb0d7386391
ChristCrett <teufteuf@windowslive.com>
2017-03-23 18:58:01 +0100

    CHrist: bcypt password in migration

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,13 +11,26 @@ class UserTableSeeder extends Seeder {
         DB::table('users')->truncate();
 
 		// 'UserSeeder'
+
+
+        User::create(array(
+            'email' => 'user@user.com',
+            'firstname' => 'aFirstName',
+            'lastname' => 'aLastName',
+            'password' => bcrypt('password'),
+            'street' => 'Rue de la rue 32 ',
+            'tel' => '027 455 32 31 ',
+            'city'=>'3980 grone',
+            'comment' => 'Veut faire des trucs bien.',
+        ));
+
         for ($k=0;$k<20;$k++)
         {
             User::create(array(
                     'email' => 'email@email.com'.$k,
                     'firstname' => 'aFirstName'.$k,
                     'lastname' => 'aLastName'.$k,
-                    'password' => 'password',
+                    'password' => bcrypt('password'),
                     'street' => 'Rue de la rue 32 ',
                     'tel' => '027 455 32 31 ',
                     'city'=>'3980 grone',
