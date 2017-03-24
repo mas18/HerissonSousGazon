ea728a438031ec07ebf2dbd4d6fb27e5519ef329
Rieille <clothilde.rieille@students.hevs.ch>
2017-03-24 09:50:36 +0100

    commit clo gui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap/bootstrap-theme.min.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
     <script>
@@ -22,31 +24,17 @@
 </head>
 <body>
     <div id="app">
+
+        <!------------- NAVBAR ----------------------->
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
-                <div class="navbar-header">
+                <div class="navbar-header"></div>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
+                <!-- Register/Login -->
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav"></ul>
+
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -3,8 +3,25 @@
 
 
 @extends('layouts.template')
+@section('header_title','page test')
+
 
 @section('main_content')
-    <div class="container" style="background-color: #2aabd2">fasdfasd</div>
-    @endsection
+    <div class="container">Middle text</div>
+
+
+    <div class="container">
+        <div class="row">
+            <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">Titre 1</article>
+            <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">Titre 2</article>
+            <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4">Titre 3</article>
+        </div>
+    </div>
+
+    <button class="btn btn-info">Mon bouton</button>
+@endsection
+
+
+
+
 
