83762ff7fd017c3e166073cccb26384f1ee37d6a
Rieille <clothilde.rieille@students.hevs.ch>
2017-04-07 13:38:30 +0200

    JS subscribe

---
+++ app/Http/Controllers/AdminSubscribeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminSubscribeController extends Controller
+{
+    //
+    public function sendUserListofSchedule($idSchedule='')
+    {
+        return "wesh ma geule";
+    }
+}
