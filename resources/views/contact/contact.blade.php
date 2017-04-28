9d92fafe6a77e46d6adc7bb930b86fa5e3271659
Rieille <clothilde.rieille@students.hevs.ch>
2017-04-28 12:52:05 +0200

    Modif ortho clo

--- resources/views/contact/contact.blade.php
+++ resources/views/contact/contact.blade.php
@@ -4,42 +4,13 @@
 @section('main_content')
 
 
-        <!-- Google MAP Script
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu5nZKbeK-WHQ70oqOWo-_4VmwOwKP9YQ"></script>
-        <script>
-            function init_map() {
-                var var_location = new google.maps.LatLng(46.1208627,7.135113899999965);
-
-                var var_mapoptions = {
-                    center: var_location,
-                    zoom: 14
-                };
-
-                var var_marker = new google.maps.Marker({
-                    position: var_location,
-                    map: var_map,
-                    title:"Hérisson sous gazon"});
-
-                var var_map = new google.maps.Map(document.getElementById("map-container"), var_mapoptions);
-
-                var_marker.setMap(var_map);
-
-            }
-            google.maps.event.addDomListener(window, 'load', init_map);
-        </script>
-
-
-        <style>
-            #map-container { height: 40%; width: 120%; }
-        </style>
-
-
-        -->
 
     <div class="row">
         <div style="padding-top: 15%;" class="col-sm-4">
             <h3>Isaline Bruchez</h3>
             <p>isaline.bruchez@gmail.com</p>
+            <p>isbruchez@hotmail.com</p>
+            <p>079/664.87.57</p>
             <a href="https://www.facebook.com/herissonsousgazon/" title="facebook">
                 <img src="pictures\fblogo.jpg" class="img-responsive" alt="Cinque Terre" style="height: 7%; width: 12%;">
             </a>
@@ -47,8 +18,8 @@
 
 
         <div style="padding-top: 5%;" class="col-sm-8">
-            <h3> Adresse </h3>
-            <p>Rue des Village 17</p>
+            <h3>Adresse de l'évènement </h3>
+            <p>Avenue de la Gare 3</p>
             <p>1906 Charrat</p>
 
             <div id="map-container" class="col-md-12; padding-left: 0;">
